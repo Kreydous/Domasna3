d98cfff5477bbe11598c05562be73392de807933
Mihail <petrushevskimihail@gmail.com>
2021-12-22 23:08:09 +0100

    Vtora verzija

--- Domasna3/Controllers/LokaciiController.cs
+++ Domasna3/Controllers/LokaciiController.cs
@@ -13,27 +13,63 @@ namespace Domasna3.Controllers
     public class LokaciiController : Controller
     {
         private lokaciiEntities2 db = new lokaciiEntities2();
-        
+        Order order = new Order();
         public ActionResult Index()
         {
             //Choose city
-            return View();
-
-        }
-
-        public ActionResult ChooseLocal()
-        {
-            List<Map> lokaciiList = new List<Map>();
+            List<string> city = new List<String>();
             var podatoci = db.mapas.ToList();
             foreach (var item in podatoci)
             {
-                lokaciiList.Add(new Map(item.name, item.lat, item.lon, item.city));
+                city.Add(item.city);
             }
+            List<string> uniqueLst = city.Distinct().ToList();
 
-            ViewBag.markerData = lokaciiList;
+            return View(uniqueLst);
 
-            return View(lokaciiList);
         }
-       
+
+        [HttpPost]
+        public ActionResult ChooseLocal(string form)
+        {
+            
+            var cityName = Request.Form["cityName"];
+            order.City = cityName;
+            List<string> locals = new List<string>();
+            var podatoci = db.mapas.ToList();
+            foreach (var item in podatoci)
+            {
+                string city = item.city;
+                if (city == null)
+                {
+                    city = "test";
+                }
+                if (city.Equals(cityName))
+                {
+                    locals.Add(item.name);
+                }
+            }
+            List<string> uniqueLst = locals.Distinct().ToList();
+            //var number = Request.Form["cityName"];
+            return View(uniqueLst);
+        }
+
+        public ActionResult ChooseFood()
+        {
+            var local = Request.Form["localName"];
+            order.Local = local;
+            List<string> menu = new List<string>();
+            menu.Add("Hamburger");
+            menu.Add("Pizza");
+            menu.Add("Pasta");
+            return View(menu);
+        }
+
+        public ActionResult Order()
+        {
+            var food = Request.Form["food"];
+            order.Food = food;
+            return View();
+        }
     }
 }
---
+++ Domasna3/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Domasna3.Models
+{
+    public class Order
+    {
+        public string City { get; set; }
+        public string Local { get; set; }
+        public string Food { get; set; }
+    }
+}
